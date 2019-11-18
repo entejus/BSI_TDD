8c2f440270bbecd1af4b3db1683d1adc87afc63f
Jakub <mielniczuk.jakub@gmail.com>
2019-11-18 19:15:37 +0100

    Adding grades for student test and implementation

--- src/com/mielniczuk/Student.java
+++ src/com/mielniczuk/Student.java
@@ -1,12 +1,22 @@
 package com.mielniczuk;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedHashMap;
+
 public class Student {
+    private static final ArrayList<Double> VALID_GRADES = new ArrayList<>(Arrays.asList(2.0, 3.0, 3.5, 4.0, 4.5, 5.0));
+
     private String firstName, surname, indexNr;
+    public LinkedHashMap<Date, Double> grades;
 
     public Student(String firstName, String surname, String indexNr) {
         this.firstName = firstName;
         this.surname = surname;
         this.indexNr = indexNr;
+        this.grades = new LinkedHashMap<>();
     }
 
     String getFirstName() {
@@ -32,4 +42,16 @@ public class Student {
     void setIndexNr(String indexNr) {
         this.indexNr = indexNr;
     }
+
+    Date addGrade(Double grade) {
+        if (VALID_GRADES.contains(grade)) {
+            Date currentDate = new Date();
+            grades.put(currentDate, grade);
+            return currentDate;
+        } else {
+            throw new IllegalArgumentException("Grade is not valid");
+        }
+    }
+
+
 }
--- src/com/mielniczuk/StudentTest.java
+++ src/com/mielniczuk/StudentTest.java
@@ -1,14 +1,21 @@
 package com.mielniczuk;
 
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 
-
+@RunWith(JUnitParamsRunner.class)
 public class StudentTest {
 
     private Student student;
@@ -19,40 +26,59 @@ public class StudentTest {
         Student s = new Student("Jan","Kowalski","D12333");
         assertNotNull(s);
     }
-
     @Test
     public void shouldReturnFirstName() {
         Student s = new Student("Jan","Kowalski","D12333");
         assertEquals("Jan",s.getFirstName());
     }
-
     @Test
     public void shouldReturnSurname() {
         Student s = new Student("Jan","Kowalski","D12333");
         assertEquals("Kowalski",s.getSurname());
     }
-
     @Test
     public void shouldReturnIndexNr() {
         Student s = new Student("Jan","Kowalski","D12333");
         assertEquals("D12333",s.getIndexNr());
     }
-
     @Test
     public void shouldSetFirstName(){
         Student s = new Student("Jan","Kowalski","D12333");
         s.setFirstName("Krzysztof");
         assertEquals(s.getFirstName(),"Krzysztof");
-    } @Test
+    }
+    @Test
     public void shouldSetSurname(){
         Student s = new Student("Jan","Kowalski","D12333");
         s.setSurname("Nowak");
         assertEquals(s.getSurname(),"Nowak");
-    } @Test
+    }
+    @Test
     public void shouldSetIndexNr(){
         Student s = new Student("Jan","Kowalski","D12333");
         s.setIndexNr("D22333");
         assertEquals(s.getIndexNr(),"D22333");
+    }
+
+    @Test
+    public void shouldSetGrade() {
+        Student s = new Student("Jan","Kowalski","D12333");
+        Date addingDate = s.addGrade(5.0);
+        assertEquals(s.grades.get(addingDate),5.0,0.1);
+    }
+    @Test
+    public void shouldReturnGradeAdditionDate(){
+        Student s = new Student("Jan","Kowalski","D12333");
+        Date addingDate = s.addGrade(5.0);
+        Date currentDate = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        assertEquals(formatter.format(addingDate),formatter.format(currentDate));
+    }
+    @Test(expected = IllegalArgumentException.class)
+    @Parameters({"0","1","-1","6","5.5","2.2","3.9"})
+    public void shouldAcceptValidGrades(Double grade){
+        Student s = new Student("Jan","Kowalski","D12333");
+        Date addingDate = s.addGrade(grade);
     }
 
 }
